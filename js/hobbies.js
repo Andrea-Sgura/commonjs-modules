29dee67b8b72a3d94e389909beef1983be660ba4
Andrea-Sgura <andreasgura96@gmail.com>
2025-04-15 07:55:46 +0200

    [UPD] Definita funzione hobbyList ed esportata

---
+++ js/hobbies.js
@@ -0,0 +1,7 @@
+// Definizione di funzione
+function hobbyList(hobbyOne, hobbyTwo, hobbyThree) {
+    return [hobbyOne, hobbyTwo, hobbyThree];
+};
+
+// Esporto la funzione
+module.exports = hobbyList;
