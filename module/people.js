ac7b09ac22af3edd654a7f398639a5302ae0c567
Andrea-Sgura <andreasgura96@gmail.com>
2025-04-15 09:14:02 +0200

    [UPD] Importate funzioni a module

---
+++ module/people.js
@@ -0,0 +1,7 @@
+// Importo le funzioni
+const firstFunction = require(`./js/names.js`);
+const secondFunction = require(`./js/hobbies.js`);
+
+// Utilizzo le funzioni
+firstFunction.person();
+secondFunction.hobbyList();
