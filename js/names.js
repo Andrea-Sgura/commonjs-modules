1710f63c6b4067e6fea154eccd761e6fc8db6819
Andrea-Sgura <andreasgura96@gmail.com>
2025-04-15 07:49:51 +0200

    [UPD] Definita funzione ed esportata

---
+++ js/names.js
@@ -0,0 +1,10 @@
+// Definizione di funzione
+function person(firstName, lastName){
+    return {
+        firstName: Paolo,
+        lastName: Bianchi
+    };
+};
+
+// Esporto la funzione
+module.exports = person;
